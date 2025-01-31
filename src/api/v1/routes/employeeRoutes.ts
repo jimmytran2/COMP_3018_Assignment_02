f78366c2177089126ed05fc7b381f250e4dd9d94
Jimmy Tran <jtran2@academic.rrc.ca>
2025-01-30 18:12:06 -0600

    test: employee routes getAllEmployees is called

--- src/api/v1/routes/employeeRoutes.ts
+++ src/api/v1/routes/employeeRoutes.ts
@@ -3,25 +3,6 @@ import * as employeeController from "../controllers/employeeControllers";
 
 const router: Router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello Employees!");
-});
-
-// example "tasks" endpoint
-/**
- * @openapi
- * /tasks:
- *  get:
- *   summary: Retrieve a list of tasks
- *   tags: [Tasks]
- *   responses:
- *    200:
- *     description: A list of tasks
- */
-router.get("/", (req, res) => {
-  res.send("Retrieve tasks");
-});
-
 router.post("/", employeeController.createEmployee);
 router.get("/", employeeController.getAllEmployees);
 
--- test/employeeRoutes.test.ts
+++ test/employeeRoutes.test.ts
@@ -1,9 +1,13 @@
 import request from "supertest";
 import app from "../src/app";
-import { createEmployee } from "../src/api/v1/controllers/employeeControllers";
+import {
+  createEmployee,
+  getAllEmployees,
+} from "../src/api/v1/controllers/employeeControllers";
 
 jest.mock("../src/api/v1/controllers/employeeControllers", () => ({
   createEmployee: jest.fn((req, res) => res.status(201).send()),
+  getAllEmployees: jest.fn((req, res) => res.status(200).send()),
 }));
 
 describe("Employee Routes", () => {
@@ -25,6 +29,13 @@ describe("Employee Routes", () => {
 
       await request(app).post("/api/v1/employees").send(mockEmployee);
       expect(createEmployee).toHaveBeenCalled();
+    });
+  });
+
+  describe("GET /api/v1/employees", () => {
+    it("should call getAllEmployees controller", async () => {
+      await request(app).get("/api/v1/employees");
+      expect(getAllEmployees).toHaveBeenCalled();
     });
   });
 });
