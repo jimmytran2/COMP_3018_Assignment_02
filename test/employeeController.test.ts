c92ea70218f5dce6a498a17d8607c769c1d2a6fc
Jimmy Tran <jtran2@academic.rrc.ca>
2025-01-30 18:18:15 -0600

    test: employee controller get all employees test

--- test/employeeController.test.ts
+++ test/employeeController.test.ts
@@ -47,4 +47,36 @@ describe("Employee Controller", () => {
       });
     });
   });
+
+  describe("getAllEmployees", () => {
+    it("should handle a successful operation", async () => {
+      const mockEmployee = [
+        {
+          id: 1,
+          name: "John Doe",
+          position: "Manager",
+          department: "Accounting",
+          email: "johndoe@pixell-river.com",
+          phone: "123-456-7890",
+          branch: 9,
+        },
+      ];
+
+      (employeeService.getAllEmployees as jest.Mock).mockResolvedValue(
+        mockEmployee
+      );
+
+      await employeeController.getAllEmployees(
+        mockReq as Request,
+        mockRes as Response,
+        mockNext
+      );
+
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith({
+        message: "Employees retrieved",
+        data: mockEmployee,
+      });
+    });
+  });
 });
