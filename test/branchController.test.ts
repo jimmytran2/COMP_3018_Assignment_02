dfa393406f84e29d98d61cd9cf932dc49eb99f79
Jimmy Tran <jtran2@academic.rrc.ca>
2025-01-31 16:24:27 -0600

    test: defined getBranchById controller test

--- test/branchController.test.ts
+++ test/branchController.test.ts
@@ -65,4 +65,29 @@ describe("Branch Controller", () => {
       });
     });
   });
+
+  describe("getBranchById", () => {
+    it("should handle a successful operation", async () => {
+      const mockBranch = {
+        id: 1,
+        name: "Vancouver Branch",
+        address: "1300 Burrard St, Vancouver, BC, V6Z 2C7",
+        phone: "604-456-0022",
+      };
+
+      (branchService.getBranchById as jest.Mock).mockResolvedValue(mockBranch);
+
+      await branchController.getBranchById(
+        mockReq as Request,
+        mockRes as Response,
+        mockNext
+      );
+
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith({
+        message: "Branch retrieved",
+        data: mockBranch,
+      });
+    });
+  });
 });
